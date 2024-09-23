74222a86398496b39c823fe60def71e48e649b41
dev1jedi <yvuavay@bk.ru>
2024-09-23 17:12:26 +0300

    Fixed netutils

--- tools/net_monitor.py
+++ tools/net_monitor.py
@@ -1,14 +1,22 @@
 import psutil
 import os
+import time
 
 def monitor_network_usage() -> int:
     net_io_counters = psutil.net_io_counters(pernic=True)
 
     interface = os.environ.get("NETINTERFACE")
 
-    bytes_sent = net_io_counters[interface].bytes_sent / (1024 * 1024)
-    bytes_recv = net_io_counters[interface].bytes_recv / (1024 * 1024)
+    start_bytes_sent = net_io_counters[interface].bytes_sent / (1024 * 1024)
+    start_bytes_recv = net_io_counters[interface].bytes_recv / (1024 * 1024)
 
-    net_usage = int(bytes_sent) + int(bytes_recv)
+    time.sleep(1)
+
+    end_bytes_sent = net_io_counters[interface].bytes_sent / (1024 * 1024)
+    end_bytes_recv = net_io_counters[interface].bytes_recv / (1024 * 1024)
+
+    sent_diff = end_bytes_sent - start_bytes_sent
+    recv_diff = end_bytes_recv - start_bytes_recv
+    net_usage = int(sent_diff) + int(recv_diff)
 
     return net_usage
